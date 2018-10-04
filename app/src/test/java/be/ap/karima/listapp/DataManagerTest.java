e0875ac8f66f8a9797c6f944dd5e38a1146ea0e4
karima-elhachimi <yuakarima@Yuas-MacBook-Pro.local>
2018-10-04 13:30:04 +0200

    Added unit tests for the datamanager methods

---
+++ app/src/test/java/be/ap/karima/listapp/DataManagerTest.java
@@ -0,0 +1,39 @@
+package be.ap.karima.listapp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DataManagerTest {
+
+    //singleton
+    DataManager dm = DataManager.getInstance();
+
+    @Test
+    public void getList() {
+        //Zien of er een lijst correct wordt doorgegeven met bestaande items.
+
+        assertSame(dm.list, dm.getList());
+
+    }
+
+    @Test
+    public void addItem() {
+        //item dat toegevoegd wordt, wordt correct opgeslgen in de lijst
+        String title = "This is a title";
+        String description = "This is a description";
+
+        MyItem item = new MyItem(title, description);
+
+        dm.addItem(item);
+
+        int indexOfAddedItem = dm.list.indexOf(item);
+
+        assertEquals(title, dm.list.get(indexOfAddedItem).getmTitle());
+        assertEquals(description, dm.list.get(indexOfAddedItem).getMyDescription());
+    }
+
+    @Test
+    public void updateItem() {
+    }
+}
