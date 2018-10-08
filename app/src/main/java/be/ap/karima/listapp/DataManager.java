becdf1f04ee153439781aabb59fdd4e9a9533260
karima-elhachimi <yuakarima@Yuas-MacBook-Pro.local>
2018-10-08 15:07:47 +0200

    added cursor handling

--- app/src/main/java/be/ap/karima/listapp/DataManager.java
+++ app/src/main/java/be/ap/karima/listapp/DataManager.java
@@ -1,8 +1,11 @@
 package be.ap.karima.listapp;
 
-import android.provider.ContactsContract;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 
 import java.util.ArrayList;
+
+import static be.ap.karima.listapp.ListDatabaseContract.*;
 
 public class DataManager {
 
@@ -12,7 +15,7 @@ public class DataManager {
             "Blackberry"), new MyItem("WebOS", "Ubuntu"), new MyItem( "Windows7", "Max OS X"), new MyItem(
             "Linux", "OS/2"), new MyItem("Ubuntu", "Windows7")};
 
-    ArrayList<MyItem> list = new ArrayList<>();
+    public static ArrayList<MyItem> list = new ArrayList<>();
 
     public static DataManager getInstance(){
         if(singleInstance == null) {
@@ -44,5 +47,35 @@ public class DataManager {
 
         int index = this.list.indexOf(item);
         this.list.set(index, item);
+    }
+
+    public static void loadFromDatabase(ListOpenHelper dbHelper){
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+        String[] myCols = new String[] {
+                ItemsEntry.ITEM_TITLE,
+                ItemsEntry.ITEM_DESCRIPTION
+        };
+
+        final Cursor myCursor = db.query(ItemsEntry.ITEMS, myCols, null, null, null, null, null );
+
+        loadDataFromCursor(myCursor);
+    }
+
+    private static void loadDataFromCursor(Cursor cursor) {
+        int titlePos = cursor.getColumnIndex(ItemsEntry.ITEM_TITLE);
+        int descriptionPos = cursor.getColumnIndex(ItemsEntry.ITEM_DESCRIPTION);
+
+        while(cursor.moveToNext()) {
+            MyItem item = new MyItem(
+                    cursor.getString(titlePos),
+                    cursor.getString(descriptionPos)
+            );
+
+            list.add(item);
+
+        }
+
+        cursor.close();
     }
 }
